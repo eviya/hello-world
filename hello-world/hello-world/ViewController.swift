5812d388a356c00a504884afae4afb6f1f2cf959
meviya <1944486008@qq.com>
2017-03-27 14:03:41 +0800

    1

--- hello-world/hello-world/ViewController.swift
+++ hello-world/hello-world/ViewController.swift
@@ -13,7 +13,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        print("hello world,UI密码")
+        print("hello world")
+        print("你好 世界")
     }
 
     override func didReceiveMemoryWarning() {
